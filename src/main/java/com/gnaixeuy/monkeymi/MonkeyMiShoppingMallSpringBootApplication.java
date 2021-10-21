af07c1d62781e5db7f6ab5afb6a297549fbb2036
GnaixEuy <sgghcj@live.com>
2021-10-21 15:34:16 +0800

    :boom: rebuild framework :bento: add css JavaScript

---
+++ src/main/java/com/gnaixeuy/monkeymi/MonkeyMiShoppingMallSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.gnaixeuy.monkeymi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyMiShoppingMallSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyMiShoppingMallSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gnaixeuy/monkeymi/MonkeyMiShoppingMallSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnaixeuy.monkeymi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeyMiShoppingMallSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
