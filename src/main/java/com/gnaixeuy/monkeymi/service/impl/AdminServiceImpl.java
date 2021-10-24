6fe72084d20558b687a0b0645061cd649ed81c92
GnaixEuy <sgghcj@live.com>
2021-10-24 17:37:31 +0800

    :sparkles: add MD5Util.java encrypt class

---
+++ src/main/java/com/gnaixeuy/monkeymi/service/impl/AdminServiceImpl.java
@@ -0,0 +1,23 @@
+package com.gnaixeuy.monkeymi.service.impl;
+
+import com.gnaixeuy.monkeymi.service.AdminService;
+
+/**
+ * <img src="http://blog.GnaixEuy.cn/wp-content/uploads/2021/08/bug.jpeg"/>
+ * <p> 管理员模块业务接口 </p>
+ * @author GnaixEuy
+ * @date 2021/10/24
+ * @see <a href='https://github.com/GnaixEuy'> GnaixEuy的GitHub </a>
+ */
+public class AdminServiceImpl implements AdminService {
+
+	/**
+	 * @param name 用户名
+	 * @param pwd 用户密码
+	 * @return loginState
+	 */
+	@Override
+	public String login(String name, String pwd) {
+		return null;
+	}
+}
