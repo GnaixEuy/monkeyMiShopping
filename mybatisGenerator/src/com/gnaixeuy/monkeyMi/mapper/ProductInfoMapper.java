d3c87f378803defb1dfa2ffbd7cbd8c0bbf6d139
GnaixEuy <sgghcj@live.com>
2021-10-26 11:41:09 +0800

    :bug: fix login.html main.html error; :lipstick: update style.css; add 404.html

---
+++ mybatisGenerator/src/com/gnaixeuy/monkeyMi/mapper/ProductInfoMapper.java
@@ -0,0 +1,31 @@
+package com.gnaixeuy.monkeyMi.mapper;
+
+import com.gnaixeuy.monkeyMi.pojo.ProductInfo;
+import com.gnaixeuy.monkeyMi.pojo.ProductInfoExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ProductInfoMapper {
+    int countByExample(ProductInfoExample example);
+
+    int deleteByExample(ProductInfoExample example);
+
+    int deleteByPrimaryKey(Integer pId);
+
+    int insert(ProductInfo record);
+
+    int insertSelective(ProductInfo record);
+
+    List<ProductInfo> selectByExample(ProductInfoExample example);
+
+    ProductInfo selectByPrimaryKey(Integer pId);
+
+    int updateByExampleSelective(@Param("record") ProductInfo record, @Param("example") ProductInfoExample example);
+
+    int updateByExample(@Param("record") ProductInfo record, @Param("example") ProductInfoExample example);
+
+    int updateByPrimaryKeySelective(ProductInfo record);
+
+    int updateByPrimaryKey(ProductInfo record);
+}
